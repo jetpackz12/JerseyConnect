455de5cc2af6f4f7fa8d4a4f75684d68a87b009c
Joseph Jett Abela <jetpackzabela12@gmail.com>
2026-07-16 12:55:59 +0800

    feat: use modal composable

---
+++ resources/js/Composables/useModal.ts
@@ -0,0 +1,21 @@
+import { ref } from "vue";
+
+export function useModal() {
+    const open = ref(false);
+    const title = ref("");
+    const type = ref("");
+    const icon = ref("");
+
+    const openModal = () => {
+        open.value = true;
+    };
+
+    const closeModal = () => {
+        open.value = false;
+        title.value = "";
+        type.value = "";
+        icon.value = "";
+    };
+
+    return { open, title, type, icon, openModal, closeModal };
+}
